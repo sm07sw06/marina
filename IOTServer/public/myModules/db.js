dc03232bd6fed8650a1c230febb3abbbee2a0118
sm07sw06 <45812390+sm07sw06@users.noreply.github.com>
2021-01-22 16:17:46 +0900

    1

--- IOTServer/public/myModules/db.js
+++ IOTServer/public/myModules/db.js
@@ -292,7 +292,8 @@ DB.prototype.SelectAnchorYN = function(mObject, callback) {
 	logger.info('  Device: ' + mObject.iD);
 
 	// 아래와 같이 .query 로 쿼리를 날릴 수 있다
-	var sQueryString  = "SELECT anchor.anchor_status FROM anchor_lidar LEFT JOIN anchor  ON anchor_lidar.anchor_id = anchor.anchor_id WHERE anchor_lidar.machine_id = '" + mObject.iD + "'";
+//	var sQueryString  = "SELECT anchor.anchor_status FROM anchor_lidar LEFT JOIN anchor  ON anchor_lidar.anchor_id = anchor.anchor_id WHERE anchor_lidar.machine_id = '" + mObject.iD + "'";
+	var sQueryString  = "SELECT 1 FROM anchor_lidar  WHERE anchor_lidar.machine_id = '" + mObject.iD + "'";
 	logger.info(sQueryString);
 	pool.query(
 		sQueryString,  (err, res) => {
