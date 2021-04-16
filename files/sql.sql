01adcd317d505ad7cd36027326a1525a53fbd040
sm07sw06 <45812390+sm07sw06@users.noreply.github.com>
2021-04-16 20:08:46 +0900

    1

--- files/sql.sql
+++ files/sql.sql
@@ -1,3 +1,19 @@
+/* 최종변경시각 트리거 */
+CREATE TRIGGER update_trigger
+BEFORE UPDATE ON tb_anchor
+FOR EACH ROW
+EXECUTE PROCEDURE update_time();
+
+
+/* 장치 수집 */
+SELECT DISTINCT machine_id,  FROM public.tb_lidardata;
+SELECT DISTINCT machine_id   FROM public.tb_boatdata;
+SELECT DISTINCT machine_id   FROM public.tb_rssidata;
+SELECT DISTINCT boat_recv_id FROM public.tb_rssidata;
+
+
+
+
 -- delete from tb_boatdata
 -- delete from tb_lidardata
 -- delete from tb_rssidata
